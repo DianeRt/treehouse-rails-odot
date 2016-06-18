3709b36f45aa69b98dad6f8dbb68e3bb6045e2ca
Diane Reta <diane.reta@gmail.com>
2016-06-18 20:47:32 +0800

    Added validations to todo items and associated tests

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,4 +3,9 @@ class TodoItem < ActiveRecord::Base
   # bin/rails generate model todo_item todo_list:references content:string
   # the references part is important to automatically add "belongs_to" in this file
   belongs_to :todo_list
+
+  # you can write validations like this:
+  # do not forget the comma after each line
+  validates :content, presence: true,
+                      length: { minimum: 2 }
 end
