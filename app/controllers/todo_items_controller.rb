08a3b0a2846e71096308451782e68d2d89eed17b
Diane Reta <diane.reta@gmail.com>
2016-06-19 10:44:07 +0800

    Add specs and code for editing todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -23,6 +23,31 @@ class TodoItemsController < ApplicationController
     end
   end
 
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.update_attributes(todo_item_params)
+      flash[:success] = "Saved todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "That todo item could not be saved."
+      render action: :edit
+    end
+  end
+
+  # In order not have to type todo_list everytime inside the controller and view
+  # Every single time it's looking for to do list ID.
+  # We can give it the ID of the to do list because we already have it.
+  # Every single time it generates a URL for us, that will be called.
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge(super)
+  end
+
   # this is important when using todo_items_params in create method to avoid error from rails
   # this is called strong parameters:
   # With strong parameters, Action Controller parameters are forbidden to be used in Active Model mass assignments until they have been whitelisted. This means that you'll have to make a conscious decision about which attributes to allow for mass update. This is a better security practice to help prevent accidentally allowing users to update sensitive model attributes.
